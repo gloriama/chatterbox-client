ca32944a0e58cf4a006277da739e9c8344ecf268
gloriama <gloria.ma@aya.yale.edu>
2015-12-07 16:50:13 -0800

    Prints messages and implements preliminary sanitization of usernames and messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,14 +1,35 @@
 // YOUR CODE HERE:
 var app = {};
 
+app.escapeMap = {
+  '&': '&amp;', 
+  '<': '&lt;',
+  '>': '&gt;',
+  '"': '&quot;',
+  "'": '&#39;',
+  '`': '&#96;',
+  ' ': '&nbsp;',
+  '!': '&#33;',
+  '@': '&#64;',
+  '$': '&#36;',
+  '%': '&#37;',
+  '(': '&#40;',
+  ')': '&#41;',
+  '=': '&#61;',
+  '+': '&#43;',
+  '{': '&#123;',
+  '}': '&#125;',
+  '[': '&#91;',
+  ']': '&#93;',
+}
 app.server = 'https://api.parse.com/1/classes/chatterbox';
-
 
 app.init = function() {
   $('.username').on("click", app.addFriend);
 
   $('#send .submit').on("submit", app.handleSubmit);
 };
+
 app.send = function(message, success) {
   $.ajax({
     type: "POST",
@@ -31,8 +52,12 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  var usernameHtml = '<a href="#" class="username">' + message.username + '</a>';
-  var newNode = $( "<p>" + usernameHtml + ": " + message.text + "</p>" );
+  var username = (typeof message.username === 'string') ? message.username : '[undefined]';
+  var text = (typeof message.text === 'string') ? message.text : '[undefined]';
+
+
+  var usernameHtml = '<a href="#" class="username">' + app._sanitize(username) + '</a>';
+  var newNode = $( "<p>" + usernameHtml + ": " + app._sanitize(text) + "</p>" );
   $('#chats').append(newNode);
 };
 
@@ -49,23 +74,47 @@ app.handleSubmit = function() {
 
 };
 
+app._printMessages = function(response) {
+  console.log("hi");
+  console.log(arguments);
+  var messages = response.results;
+  for (var i = 0; i < messages.length; i++) {
+    app.addMessage(messages[i]);
+  }
+};
+
+app._sanitize = function(str) {
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === '<') {
+      result += '&lt;';
+    } else if(str[i] === '>') {
+      result += '&gt;';
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+};
+
+//------------- sandbox --------------
+
 var message = {
   username: 'Gloria&Max Mel Brooks',
   text: 'It\'s good to be the king',
   roomname: 'lobby'
 };
 
+var Message = function(roomname, text, username) {
+  this.opponents = {};
+  this.roomname = "Gloria and Max";
+  this.text = text;
+  this.username = 'rhodia';
+};
 
-// var Message = function(str) {
-//   this.createdAt = '';
-//   this.objectId = '';
-//   this.opponents = {};
-//   this.roomname = "Gloria and Max";
-//   this.text = str;
-//   this.updatedAt = '';
-//   this.username = 'rhodia';
-// };
-
-app.send(message, function() { console.log("ok"); });
+//app.send(message, function() { console.log("ok"); });
 //$.post('https://api.parse.com/1/classes/chatterbox', new Message("test from Max and Gloria"), function() {console.log("success");});
-$.get('https://api.parse.com/1/classes/chatterbox', 'abc', function() {console.log(arguments)});
+
+
+
+$.get('https://api.parse.com/1/classes/chatterbox', 'abc', app._printMessages);
